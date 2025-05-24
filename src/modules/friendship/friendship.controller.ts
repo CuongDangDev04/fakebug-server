acd369a35b30476ad21049d8dc0598bfb4679005
BlueSkyDev04 <cuongdang.270920@gmail.com>
2025-05-24 22:07:22 +0700

    unf, block, unblock, getAllgetReceivedFriendRequest,getSentFriendRequests, cancelSentRequest

--- src/modules/friendship/friendship.controller.ts
+++ src/modules/friendship/friendship.controller.ts
@@ -1,18 +1,18 @@
-import { Controller, Post, Body, Param, ParseIntPipe, Get, UseGuards, Req } from '@nestjs/common';
+import { Controller, Post, Body, Param, ParseIntPipe, Get, UseGuards, Req, Delete } from '@nestjs/common';
 import { FriendshipService } from './friendship.service';
 import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
 
+
+@UseGuards(JwtAuthGuard)
 @Controller('friendships')
 export class FriendshipController {
-  constructor(private readonly friendshipService: FriendshipService) {}
-
-  @UseGuards(JwtAuthGuard)
+  constructor(private readonly friendshipService: FriendshipService) { }
+  //gửi kb
   @Post('send-request/:receiverId')
   async sendRequest(@Req() req, @Param('receiverId', ParseIntPipe) receiverId: number) {
     return this.friendshipService.sendFriendRequest(req.user.userId, receiverId);
   }
-
-  @UseGuards(JwtAuthGuard)
+  //phản hồi lời mời kb
   @Post('respond/:requestId')
   async respondRequest(
     @Req() req,
@@ -22,9 +22,39 @@ export class FriendshipController {
     return this.friendshipService.respondFriendRequest(requestId, req.user.userId, accept);
   }
 
-  @UseGuards(JwtAuthGuard)
   @Get('friends')
   async getFriends(@Req() req) {
     return this.friendshipService.getFriends(req.user.userId);
+  }
+
+  @Delete('unfriend/:targetId')
+  async unfriend(@Param("targetId") targetId: number, @Req() req: any) {
+    return this.friendshipService.unfriend(req.user.id, +targetId)
+  }
+  @Post('block/:targetId')
+  async block(@Param('targetId') targetId: number, @Req() req: any) {
+    return this.friendshipService.blockUser(req.user.id, +targetId)
+  }
+
+  @Delete('unblock/:targetId')
+  async unblockUser(@Req() req: any, @Param('targetId') targetId: number) {
+    return this.friendshipService.unblockUser(req.user.id, +targetId);
+  }
+  //  Danh sách lời mời đã nhận
+  @Get('requests/received')
+  async getReceivedRequests(@Req() req: any) {
+    return this.friendshipService.getReceivedFriendRequests(req.user.id);
+  }
+
+  // Danh sách lời mời đã gửi
+  @Get('requests/sent')
+  async getSentRequests(@Req() req: any) {
+    return this.friendshipService.getSentFriendRequests(req.user.id);
+  }
+
+  //  Hủy lời mời kết bạn đã gửi
+  @Delete('requests/cancel/:targetId')
+  async cancelSentRequest(@Req() req: any, @Param('targetId') targetId: number) {
+    return this.friendshipService.cancelSentRequest(req.user.id, +targetId);
   }
 }
--- src/modules/friendship/friendship.service.ts
+++ src/modules/friendship/friendship.service.ts
@@ -12,7 +12,7 @@ export class FriendshipService {
     @InjectRepository(User)
     private userRepo: Repository<User>,
   ) { }
-
+  //gửi lời mời kb
   async sendFriendRequest(senderId: number, receiverId: number) {
     if (senderId === receiverId) {
       throw new BadRequestException('Bạn không thể gửi lời mời kết bạn cho chính mình.');
@@ -70,7 +70,7 @@ export class FriendshipService {
       }
     };
   }
-
+  //phản hồi lời mời kb
   async respondFriendRequest(requestId: number, userId: number, accept: boolean) {
     const request = await this.friendshipRepo.findOne({
       where: { id: requestId },
@@ -100,8 +100,6 @@ export class FriendshipService {
       };
     }
   }
-
-
   async getFriends(userId: number) {
     const friendships = await this.friendshipRepo.find({
       where: [
@@ -111,7 +109,7 @@ export class FriendshipService {
       relations: ['userOne', 'userTwo'],
     });
 
-    return friendships.map(friendship => {
+    const friends = friendships.map(friendship => {
       const friend =
         friendship.userOne.id === userId ? friendship.userTwo : friendship.userOne;
 
@@ -124,6 +122,154 @@ export class FriendshipService {
         created_at: friendship.created_at,
       };
     });
+    return {
+      totalFriends: friends.length,
+      friends,
+    };
+  }
+  async unfriend(userId: number, targerId: number) {
+    const friendship = await this.friendshipRepo.findOne({
+      where: [
+        {
+          userOne: { id: userId }, userTwo: { id: targerId }, status: 'accepted'
+        },
+        {
+          userOne: { id: targerId }, userTwo: { id: userId }, status: 'accepted'
+        }
+      ]
+    })
+    if (!friendship) {
+      throw new NotFoundException("Hai người không phải bạn bè")
+    }
+    await this.friendshipRepo.remove(friendship)
+    return {
+      message: "Đã huỷ kết bạn thành công"
+    }
+  }
+  async blockUser(requestId: number, targetId: number) {
+    if (requestId === targetId) {
+      throw new BadRequestException("Không thể tự chặn chính mình");
+    }
+
+    const requester = await this.userRepo.findOne({ where: { id: requestId } });
+    const target = await this.userRepo.findOne({ where: { id: targetId } });
+
+    if (!requester || !target) {
+      throw new NotFoundException("Người dùng không tồn tại");
+    }
+
+    let friendship = await this.friendshipRepo.findOne({
+      where: [
+        { userOne: { id: requestId }, userTwo: { id: targetId } },
+        { userOne: { id: targetId }, userTwo: { id: requestId } }
+      ],
+      relations: ['userOne', 'userTwo'],
+    });
+
+    if (friendship) {
+      if (friendship.status === 'blocked') {
+        throw new BadRequestException("Bạn đã chặn người này trước đó");
+      }
+
+      friendship.status = 'blocked';
+    } else {
+      friendship = this.friendshipRepo.create({
+        userOne: requester!,
+        userTwo: target!,
+        status: 'blocked',
+      });
+    }
+
+    await this.friendshipRepo.save(friendship);
+    return { message: 'Đã chặn người dùng thành công' };
+  }
+  async unblockUser(requestId: number, targetId: number) {
+    const friendship = await this.friendshipRepo.findOne({
+      where: [
+        { userOne: { id: requestId }, userTwo: { id: targetId }, status: 'blocked' },
+        { userOne: { id: targetId }, userTwo: { id: requestId }, status: 'blocked' },
+      ],
+      relations: ['userOne', 'userTwo'],
+    });
+
+    if (!friendship) {
+      throw new NotFoundException("Không có người dùng bị chặn");
+    }
+
+    await this.friendshipRepo.remove(friendship);
+    return {
+      message: "Đã bỏ chặn người dùng",
+    };
+  }
+  //danh sách lời mời kết bạn đã nhận
+  async getReceivedFriendRequests(userId: number) {
+    const requests = await this.friendshipRepo.find({
+      where: {
+        userTwo: { id: userId },
+        status: 'pending',
+      },
+      relations: ['userOne'],
+    });
+
+    const formatted = requests.map(r => ({
+      id: r.id,
+      from: {
+        id: r.userOne.id,
+        firstName: r.userOne.first_name,
+        lastName: r.userOne.last_name,
+        avatar: r.userOne.avatar_url,
+      },
+      created_at: r.created_at,
+    }));
+
+    return {
+      total: formatted.length,
+      requests: formatted,
+    };
+  }
+  //danh sách lời mời đã gửi
+  async getSentFriendRequests(userId: number) {
+    const requests = await this.friendshipRepo.find({
+      where: {
+        userOne: { id: userId },
+        status: 'pending',
+      },
+      relations: ['userTwo'],
+    });
+
+    const formatted = requests.map(r => ({
+      id: r.id,
+      to: {
+        id: r.userTwo.id,
+        firstName: r.userTwo.first_name,
+        lastName: r.userTwo.last_name,
+        avatar: r.userTwo.avatar_url,
+      },
+      created_at: r.created_at,
+    }));
+    return {
+      total: formatted.length,
+      requests: formatted,
+    }
+  }
+  //Hủy lời mời kết bạn đã gửi
+  async cancelSentRequest(senderId: number, receiverId: number) {
+    const request = await this.friendshipRepo.findOne({
+      where: {
+        userOne: { id: senderId },
+        userTwo: { id: receiverId },
+        status: 'pending',
+      },
+    });
+
+    if (!request) {
+      throw new NotFoundException("Không tìm thấy lời mời đã gửi");
+    }
+
+    await this.friendshipRepo.remove(request);
+    return {
+      message: "Đã hủy lời mời kết bạn",
+    };
   }
 
 }
